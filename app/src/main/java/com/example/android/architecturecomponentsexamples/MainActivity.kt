cf064f9037462706fb4a4e76ccd0df2e750ee099
Winsey Li <winsey@storypark.com>
2022-05-19 13:58:30 +1200

    add a button in MainActivity to start another activity of view binding

--- app/src/main/java/com/example/android/architecturecomponentsexamples/MainActivity.kt
+++ app/src/main/java/com/example/android/architecturecomponentsexamples/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.android.architecturecomponentsexamples
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.android.architecturecomponentsexamples.viewbinding.ViewBindingExampleActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewBindingExampleButton = findViewById<Button>(R.id.button_view_binding_example)
+        viewBindingExampleButton.setOnClickListener {
+            openViewBindingExample()
+        }
+    }
+
+    private fun openViewBindingExample() {
+        val intent = Intent(this, ViewBindingExampleActivity::class.java)
+        startActivity(intent)
     }
 }
